37a10d037965c5f32e6bbcb5891ea9eccbb44e65
ctrlkva <145870188+ctrlkva@users.noreply.github.com>
2024-07-20 19:15:55 +0500

    Add files via upload

---
+++ kurs8/MyForm.h
@@ -0,0 +1,358 @@
+﻿#pragma once
+#include <cmath>
+#include <string>
+using std::string;
+namespace kurs8 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+
+
+	private: System::Windows::Forms::Label^ label2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+
+
+
+
+	private:
+
+
+
+
+
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label2
+			// 
+			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(46, 25);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(182, 17);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Формулировка задачи:";
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(177, 284);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(100, 26);
+			this->button2->TabIndex = 37;
+			this->button2->Text = L"Очистить";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label9
+			// 
+			this->label9->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label9->AutoSize = true;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label9->Location = System::Drawing::Point(46, 59);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(274, 17);
+			this->label9->TabIndex = 38;
+			this->label9->Text = L"Среднее кол-во звонков в минуту ( λ ) = ";
+			// 
+			// label10
+			// 
+			this->label10->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label10->AutoSize = true;
+			this->label10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label10->Location = System::Drawing::Point(46, 91);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(203, 153);
+			this->label10->TabIndex = 39;
+			this->label10->Text = L"Найти вероятность того, что:\r\n\r\nза время t =        \r\n\r\nпоступит x заявок;\r\nпосту"
+				L"пит > x заявок;\r\nпоступит >= x заявок;\r\nпоступит < x заявок;\r\nпоступит <= x заяв"
+				L"ок.";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(316, 56);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(51, 23);
+			this->textBox1->TabIndex = 40;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(132, 122);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(52, 23);
+			this->textBox2->TabIndex = 41;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox3->Location = System::Drawing::Point(272, 122);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(48, 23);
+			this->textBox3->TabIndex = 42;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->Location = System::Drawing::Point(49, 284);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(100, 26);
+			this->button3->TabIndex = 43;
+			this->button3->Text = L"Рассчитать";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// label1
+			// 
+			this->label1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(247, 125);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(30, 17);
+			this->label1->TabIndex = 44;
+			this->label1->Text = L"x = ";
+			// 
+			// label3
+			// 
+			this->label3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(212, 159);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(16, 85);
+			this->label3->TabIndex = 45;
+			this->label3->Text = L"=\r\n=\r\n=\r\n=\r\n=";
+			// 
+			// label4
+			// 
+			this->label4->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(247, 159);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(12, 17);
+			this->label4->TabIndex = 46;
+			this->label4->Text = L" ";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.980198F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(303, 284);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(118, 26);
+			this->button1->TabIndex = 47;
+			this->button1->Text = L"О программе";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(470, 353);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label1);
+			this->HelpButton = true;
+			this->MaximizeBox = false;
+			this->MaximumSize = System::Drawing::Size(486, 393);
+			this->MinimumSize = System::Drawing::Size(486, 393);
+			this->Name = L"MyForm";
+			this->ShowIcon = false;
+			this->Text = L"Распределение Пуассона в СМО";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		double l = Convert::ToDouble(textBox1->Text);
+		double t = Convert::ToDouble(textBox2->Text);
+		int n = Convert::ToInt32(textBox3->Text);
+		double p, p1, p3, p2; double p5 = 0; double p4 = 0; double f = 1; double lt = l * t;
+		double p0 = exp(-lt);
+		for (int i = 1; i <= n; i++)
+		{
+			f = f * i;
+		}
+		p1 = (pow(lt, n) * exp(-lt)) / f;
+		f = 1;
+		for (int i = 1; i < n; i++)
+		{
+			f = f * i;
+			p = (pow((lt), i) * exp(-lt)) / f;
+			p4 = p4 + p;
+		}
+		p4 = p4 + p0;
+		p3 = 1 - p4;
+		for (int i = 1; i <= n; i++)
+		{
+			f = f * i;
+			p = (pow((lt), i) * exp(-lt)) / f;
+			p5 = p5 + p;
+		}
+		p5 = p5 + p0;
+		p2 = 1 - p5;
+		label4->Text = Convert::ToString(L"P( x ) = ");
+		label4->Text += Convert::ToString(round(p1 * 1000) / 1000 + L"\r\nP( > x ) = " + round(p2 * 1000) / 1000 + L"\r\nP( ≥ x ) = " + round(p3 * 1000) / 1000 + L"\r\nP( < x ) = " + round(p4 * 1000) / 1000 + L"\r\nP( ≤ x ) = " + round(p5 * 1000) / 1000);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		textBox1->Clear();
+		textBox2->Clear();
+		textBox3->Clear();
+		label4->Text = Convert::ToString(" ");
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		MessageBox::Show("Программа создана студентом 083 группы.\n" + "Задание выполнено для курсового проекта.\n"
+			+ "Автор: Абдураимова Анастасия.\n\n" + "Программа принимает целые и дробные значения.\nПосле того, как вы введете значение, нажмите на кнопку\nрассчитать. Перед вами появится результат.\nЧтобы рассчитать другую задачу, нажмите на кнопку\nочистить. Повторите предыдущие действия.",
+	"О программе", MessageBoxButtons::OK);
+	}
+};
+}
